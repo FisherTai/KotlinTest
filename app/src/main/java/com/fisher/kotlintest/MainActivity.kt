cb6a56579412f7aa1193996a72c271533e1c16bb
Fisher.Tai <fisher.tai@email.com>
2020-09-07 18:02:23 +0800

    First Commit 練習Kotlin用的專案

---
+++ app/src/main/java/com/fisher/kotlintest/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.fisher.kotlintest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+    override fun onClick(p0: View?) {
+
+    }
+}
