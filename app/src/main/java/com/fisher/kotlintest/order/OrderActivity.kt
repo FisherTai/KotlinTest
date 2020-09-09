9493c1154076db82b123c5bbeb6e314982a9fcb7
Fisher.Tai <fisher.tai@email.com>
2020-09-09 18:04:43 +0800

    9/9

---
+++ app/src/main/java/com/fisher/kotlintest/order/OrderActivity.kt
@@ -0,0 +1,11 @@
+package com.fisher.kotlintest.order
+
+import com.fisher.kotlintest.BaseActivity
+import com.fisher.kotlintest.R
+
+class OrderActivity :BaseActivity(){
+    override fun getLayoutView(): Int = R.layout.activity_order
+
+    override fun handleView() {
+    }
+}
